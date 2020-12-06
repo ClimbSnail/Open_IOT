6a0c9090336c46c8aacf9c52abc785c5eb617459
ClimbSnail <hqclimb@gmail.com>
2020-12-06 21:22:41 +0800

    project start

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Open_IOT
-个人物联网方案整合
+项目目的：打造最丰富的物联网控制系统
+
+## ESP8266介绍
+![ESP6288_Model_GPIO](./Image/ESP8266_Model_GPIO.png)
+![ESP6288_Model_GPIO](https://gitee.com/ClimbSnailQ/Project_Image/raw/master/Open_IOT/ESP8266_Model_GPIO.png)
+
+## 学习资料传送门
+1. [教程 in Github](https://github.com/lvidarte/esp8266/wiki)
+2. [ESP8266固件](http://micropython.org/download#esp8266)
+3. [IDE](http://docs.dfrobot.com.cn/upycraft/)
+4. [快速开发指南](https://micropython.nxez.com/2019/01/04/esp8266-quick-development-guide.html)
+5. [官方参考文档](https://docs.micropython.org/en/latest/esp8266/quickref.html#pins-and-gpio)
---
+++ setup_web.py
@@ -0,0 +1,71 @@
+import socket
+from machine import Pin
+from re import search
+from web import do_connect
+
+html = """
+<html>
+  <head>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LED控制</title>
+  </head>
+  <body>
+    <p>Hello World!!!</p>
+  </body>
+</html>
+"""
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect('wifi名', '密码')  # wifi的SSID和密码
+    while not sta_if.isconnected():
+        pass
+    print('network config:', sta_if.ifconfig())
+    return sta_if
+
+wlan = do_connect()
+ip = wlan.ifconfig()[0]
+port = 80
+led = Pin(2, Pin.OUT)
+webserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建套接字
+webserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 设置给定套接字选项的值
+webserver.bind((ip, port))  # 绑定IP地址和端口号
+webserver.listen(5)  # 监听套接字
+print("服务器地址:{}:{}".format(ip, port))
+
+while True:
+    conn, addr = webserver.accept()
+    request = conn.recv(1024)
+    if len(request) > 0:
+        request = request.decode()
+        result = search("(.*?) (.*?) HTTP/1.1", request)
+        if result:
+            method = result.group(1)
+            url = result.group(2)
+            print(url)
+            conn.send("HTTP/1.1 200 OK\r\n")
+            conn.send("Server: Esp8266\r\n")
+            conn.send("Content-Type: text/html;charset=UTF-8\r\n")
+            conn.send("Connection: close\r\n")
+            conn.send("\r\n")
+            if url == "/led1":
+                led.value(1)
+                conn.send("灯灭")
+            elif url == "/led2":
+                led.value(0)
+                conn.send("灯亮")
+            else:
+                conn.sendall(html)
+            conn.send("\r\n")  # 发送结束
+        else:
+            print("not found url")
+    else:
+        print("no request")
+    conn.close()
